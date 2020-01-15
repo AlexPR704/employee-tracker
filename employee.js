21b8e7b1e2d784eec408a27e05614bfeb9cf57cb
Alexander Garcia <alexg97343@gmail.com>
2020-01-15 16:59:43 -0500

    starting the promt

--- employee.js
+++ employee.js
@@ -19,3 +19,43 @@ connection.connect(function(err) {
   if (err) throw err;
   runSearch();
 });
+
+function runSearch() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View all departments",
+        "View all employees by department",
+        "View all roles by employees",
+        "Add a department",
+        "Add an employee",
+        "Add a role to employee"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "Find songs by artist":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Find artists with a top song and top album in the same year":
+        songAndAlbumSearch();
+        break;
+      }
+    });
+}
